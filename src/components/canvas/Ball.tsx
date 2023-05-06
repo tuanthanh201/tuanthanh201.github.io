07bc9e4e2a4a770f30ff98303cf95f8aff14de77
Thanh Nguyen <ngthanh2248@gmail.com>
2023-05-05 21:26:11 -0700

    Load earth model

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -19,34 +19,34 @@ const Ball = ({ icon }: BallProps) => {
 
 	return (
 		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+			<ambientLight intensity={0.25} />
+			<directionalLight position={[0, 0, 0.05]} />
+			<mesh castShadow receiveShadow scale={2.75}>
+				<icosahedronGeometry args={[1, 1]} />
+				<meshStandardMaterial
+					color='#fff8eb'
+					polygonOffset
+					polygonOffsetFactor={-5}
+					flatShading
+				/>
+				<Decal
+					position={[0, 0, 1]}
+					rotation={[2 * Math.PI, 0, 6.25]}
+					scale={1}
+					map={decal}
+					flatShading
+				/>
+			</mesh>
+		</Float>
 	);
 };
 
 const BallCanvas = ({ icon }: BallProps) => {
 	return (
 		<Canvas
-      className='cursor-pointer'
+			className='cursor-pointer'
 			frameloop='always'
-      dpr={[1, 2]}
+			dpr={[1, 2]}
 			gl={{
 				preserveDrawingBuffer: true,
 			}}
